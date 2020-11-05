f6dbcbd33c126fd8c33086449f4eba0fd2a9900e
jellier@163.com <jellier@163.com>
2020-11-05 22:51:48 +0800

    update

---
+++ temp/nestLoop.py
@@ -0,0 +1,21 @@
+print('==No.1==')
+for i in range(4):
+    f = '草莓'
+    print(f)
+
+print('==No.2==')
+for i in range(2):
+    for j in range(2):
+        f = '草莓'
+        print(f)
+
+print('==No.3==')
+for i in range(6):
+    if i % 2 == 0:
+        f = '草莓'
+    else:
+        f = '芒果'
+
+    for j in range(6):
+        print(f)
+
